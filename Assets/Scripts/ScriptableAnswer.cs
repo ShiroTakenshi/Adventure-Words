5be0c14c40dadd024af80e804292958b05c906bf
Arriziq <114041758+Arriziqrzyx@users.noreply.github.com>
2022-12-01 21:10:44 +0700

    fix

--- Assets/Scripts/ScriptableAnswer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "QuestionsData", menuName = "QuestionsData", order = 1)] 
-public class ScriptableAnswer : ScriptableObject
-{
-    public List<QuestionData> questions;
-}
